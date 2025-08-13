9a04701f905d1edd2ab46c5ffb75631a156d0ea5
DELL <DELL@DESKTOP-F61RFC2>
2025-08-13 17:02:59 +0530

    initial

---
+++ first/src/first/ascii.java
@@ -0,0 +1,13 @@
+package first;
+
+public class ascii {
+	
+	    public static void main(String[] args) {
+	        int[] arr = {65, 66, 67, 68}; // ASCII A=65, B=66, etc.
+	        for (int n : arr) {
+	            System.out.print((char)n + " ");
+	        }
+	    }
+	}
+
+
---
+++ first/src/first/remove.java
@@ -0,0 +1,14 @@
+package first;
+import java.util.LinkedHashSet;
+
+public class remove {
+	
+	    public static void main(String[] args) {
+	        int[] arr = {12, 34, 12, 45, 67, 89};
+	        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+	        for (int n : arr) set.add(n);
+	        System.out.print("O/P: ");
+	        System.out.println(set);
+	    }
+	}
+
